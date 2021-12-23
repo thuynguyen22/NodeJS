2b1aa554de314c0f45ae37bbe3908c548ac1f676
thuynguyen22 <thuy.nguyen22@student.passerellesnumeriques.org>
2021-12-23 14:25:38 +0700

    Node Js

---
+++ hello.js
@@ -0,0 +1,25 @@
+// const http = require("http");
+
+// const hostname = "127.0.0.1";
+// const port = 8000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello World! toi la thuy");
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
