50efb36c66d7cea161a6f058616b4cb743cc641b
thuynguyen22 <thuy.nguyen22@student.passerellesnumeriques.org>
2021-12-28 09:27:44 +0700

    part_5

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -17,13 +17,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 ////////////////////
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
-////////////////////
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -36,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
